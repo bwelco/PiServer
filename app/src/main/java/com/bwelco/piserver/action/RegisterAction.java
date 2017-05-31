41907c1a4f38e83e815a997b1e377fd65c777f82
bwelco <1143955880@qq.com>
2017-05-31 19:59:38 +0800

    update register action

--- app/src/main/java/com/bwelco/piserver/action/RegisterAction.java
+++ app/src/main/java/com/bwelco/piserver/action/RegisterAction.java
@@ -1,7 +1,9 @@
 package com.bwelco.piserver.action;
 
+import com.bwelco.piserver.UserBean;
 import com.bwelco.piserver.util.UserSpUtil;
 
+import java.util.List;
 import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
@@ -17,6 +19,14 @@ public class RegisterAction extends BaseAction {
         String username = queryMap.get("user");
         String passwd = queryMap.get("pass");
         String applyReason = queryMap.get("apply_info");
+
+        List<UserBean> users = UserSpUtil.getUserList();
+        for (UserBean userBean : users) {
+            if (userBean.username.equals(username)) {
+                return getDefaultResponse(false, "用户名已存在");
+            }
+        }
+
         UserSpUtil.addUser(username, passwd, applyReason);
         return getDefaultResponse(true, "成功发送申请");
     }
